aa302bbaa882d86fa85bb2b643b2f8cf2f5b6aaa
ccjr <ccarneiroj@gmail.com>
2008-10-14 08:04:13 +0400

    Created proper partials

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,5 @@
 class CardsController < ApplicationController
+  def show
+    @card = Card.find params[:id]
+  end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,8 @@
 class Card < ActiveRecord::Base
   validates_presence_of :number, :title
+  named_scope :races, :select => 'distinct race', :order => 'race'
+  
+  def first_letter_of_title
+    title.first
+  end
 end
