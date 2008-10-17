dda036ae13105fe7fe5d800b746bd95af1149d6a
ccjr <ccarneiroj@gmail.com>
2008-10-17 13:32:39 +0400

    Added search by affiliation

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,10 +2,13 @@ class Card < ActiveRecord::Base
   validates_presence_of :number, :title
 
   named_scope :races, :select => 'distinct race', :order => 'race'
+  named_scope :affiliations, :select => 'distinct affiliation', :order => 'affiliation', :conditions => 'affiliation IS NOT NULL'
+  
   named_scope :where_title, lambda {|title| { :conditions => ["cards.title like ?", "%#{title}%"] }}
   named_scope :where_number, lambda {|number| { :conditions => ["cards.number = ?", number] }}
   named_scope :where_element, lambda {|element| { :conditions => ["cards.element = ?", element] }}
   named_scope :where_race, lambda {|race| { :conditions => ["cards.race = ?", race] }}
+  named_scope :where_affiliation, lambda {|affiliation| { :conditions => ["cards.affiliation = ?", affiliation] }}
   
   ELEMENTS = %w{ Biolith Earth Fire Water Wood Spell }
 
@@ -15,6 +18,7 @@ class Card < ActiveRecord::Base
     listings = (listings || self).where_number(options[:number]) unless options[:number].blank?
     listings = (listings || self).where_element(options[:element]) unless options[:element].blank?
     listings = (listings || self).where_race(options[:race]) unless options[:race].blank?
+    listings = (listings || self).where_affiliation(options[:affiliation]) unless options[:affiliation].blank?
     return (listings || self).all(:order => order)
   end
   
