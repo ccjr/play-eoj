3dbca13c9ffdc9e716053adeca55dad3cbd09ea3
rbarazi <rbarazi@gmail.com>
2008-10-22 01:01:18 +0400

    Installing and implementing HopToad

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '93a408b2a07d69e489138242c5f5d0dc'
   
+  include HoptoadNotifier::Catcher
+  
   def no_iphone_page
     unless request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]
       redirect_to '/no-iphone.html'
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = '808643ecdb88a2a66a23c0ab0d969d09'
+end
