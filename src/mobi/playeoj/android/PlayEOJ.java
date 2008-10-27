bcbd67a3d61693b40b5d2fa40a296e17fae52152
ccjr <ccarneiroj@gmail.com>
2008-10-27 15:44:49 +0400

    Getting one of the menu items description from resources

--- src/mobi/playeoj/android/PlayEOJ.java
+++ src/mobi/playeoj/android/PlayEOJ.java
@@ -11,16 +11,16 @@ public class PlayEOJ extends ListActivity {
 	
     private static final int MENU_ALL_CARDS = 0;
 
-    private String[] mStrings = {
-     	"All Cards", "By Element", "By Race", "By Affiliation", "Glossary", "About"
-    };
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        String[] menuStrings =  {
+             	"All Cards", "By Element", "By Race", "By Affiliation", "Glossary", getString(R.string.about)
+        };
+        
         // Use an existing ListAdapter that will map an array of strings to TextViews
-        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStrings));
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuStrings));
         getListView().setTextFilterEnabled(true);
     }
 
