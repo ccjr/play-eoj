5f2f8148fe2acc7961c5b56bfd459505bcb2353e
ccjr <ccarneiroj@gmail.com>
2008-10-27 15:14:57 +0400

    App launching with basic list menu

--- src/mobi/playeoj/android/PlayEOJ.java
+++ src/mobi/playeoj/android/PlayEOJ.java
@@ -1,13 +1,23 @@
 package mobi.playeoj.android;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class PlayEOJ extends Activity {
-    /** Called when the activity is first created. */
+public class PlayEOJ extends ListActivity {
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        // Use an existing ListAdapter that will map an array
+        // of strings to TextViews
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mStrings));
+        getListView().setTextFilterEnabled(true);
     }
+
+    private String[] mStrings = {
+    	"All Cards", "By Element", "By Race", "By Affiliation", "Glossary", "About"
+    };
 }
