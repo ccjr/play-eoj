280ebe5e19fc5933ff6c2095f3b63a2d56551859
ccjr <ccarneiroj@gmail.com>
2008-11-11 22:56:27 +0400

    Loading an html file with a WebView object

--- src/mobi/playeoj/android/About.java
+++ src/mobi/playeoj/android/About.java
@@ -2,13 +2,26 @@ package mobi.playeoj.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class About extends Activity {
+
+    private WebView mWebView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         this.setTitle("Play EoJ >> About");
         setContentView(R.layout.about);
+
+        mWebView = (WebView)findViewById(R.id.webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setSavePassword(false);
+        webSettings.setSaveFormData(false);
+        webSettings.setJavaScriptEnabled(false);
+        webSettings.setSupportZoom(false);
+
+        mWebView.loadUrl("file:///android_asset/about.html");
     }
 }
