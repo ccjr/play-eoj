56bd88bbee308eb95888c5d33495694230ab7d53
ccjr <ccarneiroj@gmail.com>
2008-11-19 18:10:28 +0400

    Added elements to show card activity

--- src/mobi/playeoj/android/R.java
+++ src/mobi/playeoj/android/R.java
@@ -11,10 +11,23 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int card=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int biolith=0x7f020000;
+        public static final int card=0x7f020001;
+        public static final int earth=0x7f020002;
+        public static final int fire=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int none=0x7f020005;
+        public static final int spell=0x7f020006;
+        public static final int water=0x7f020007;
+        public static final int wood=0x7f020008;
     }
     public static final class id {
+        public static final int card_activation_cost=0x7f050004;
+        public static final int card_description=0x7f050006;
+        public static final int card_element=0x7f050002;
+        public static final int card_race=0x7f050005;
+        public static final int card_summoning_cost=0x7f050003;
+        public static final int card_title=0x7f050001;
         public static final int webview=0x7f050000;
     }
     public static final class layout {
--- src/mobi/playeoj/android/ShowCard.java
+++ src/mobi/playeoj/android/ShowCard.java
@@ -1,13 +1,48 @@
 package mobi.playeoj.android;
 
+import mobi.playeoj.android.model.Card;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ShowCard extends Activity {
+
+    Card mCard;
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        this.setTitle("Card >> " + this.getIntent().getExtras().getString("title"));
+        mCard = Card.findByTitle(this.getIntent().getExtras().getString("title")); 
+        
+        this.setTitle("Card >> " + mCard.getTitle());
         setContentView(R.layout.show_card);
+        
+        // set the elements to display all information
+        ((TextView)findViewById(R.id.card_title)).setText(mCard.getTitle() + " - #" + mCard.getNumber());
+        ((ImageView)findViewById(R.id.card_element)).setImageResource(setElementImage());
+
+        ((TextView)findViewById(R.id.card_summoning_cost)).setText("Summoning: " + mCard.getSummoning_cost());
+        ((TextView)findViewById(R.id.card_activation_cost)).setText("Activation: " + mCard.getActivation_cost());
+        
+        ((TextView)findViewById(R.id.card_race)).setText(mCard.getRace() + " / " + mCard.getAffiliation());
+        ((TextView)findViewById(R.id.card_description)).setText(mCard.getBody());
+    }
+    
+    private int setElementImage() {
+	if (mCard.getElement().equals("Fire")) {
+	    return R.drawable.fire;
+	} else if (mCard.getElement().equals("Water")) {
+	    return R.drawable.water;
+	} else if (mCard.getElement().equals("Wood")) {
+	    return R.drawable.wood;
+	} else if (mCard.getElement().equals("Earth")) {
+	    return R.drawable.earth;
+	} else if (mCard.getElement().equals("Biolith")) {
+	    return R.drawable.biolith;
+	} else if (mCard.getElement().equals("Spell")) {
+	    return R.drawable.spell;
+	} else {
+	    return R.drawable.none;
+	}
     }
 }
